f692efbb73ac98438e9225da88adf53419dd5b2d
Roby Julian <cdevrywhr@gmail.com>
2023-08-23 23:12:28 +0700

    change url ig

--- components/Header/index.js
+++ components/Header/index.js
@@ -33,8 +33,8 @@ const Header = ({ handleWorkScroll, handleAboutScroll }) => {
               <div className="flex items-center"> 
                {data.darkMode && (
                   <Button
-                    onClick={() =>
-                      setTheme(theme === "dark" ? "light" : "dark")
+                     onClick={() =>
+                        window.open("https://instagram.com/imrobwe")
                     }
                   >
                     <img
@@ -81,7 +81,9 @@ const Header = ({ handleWorkScroll, handleAboutScroll }) => {
 
             {mounted && theme && data.darkMode && (
               <Button
-                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                 onClick={() =>
+                        window.open("https://instagram.com/imrobwe")
+                    }
               >
                 <img
                   className="h-6"
